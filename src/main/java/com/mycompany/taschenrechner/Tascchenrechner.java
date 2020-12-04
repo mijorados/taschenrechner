17c64d9d47a93c347414cb2e919023d538949e74
Mijo Rados <mijo.rados@hotmail.com>
2020-12-04 20:49:17 +0100

    Initial commit

---
+++ src/main/java/com/mycompany/taschenrechner/Tascchenrechner.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.taschenrechner;
+
+
+public class Tascchenrechner {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
