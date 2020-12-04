4b59053256308050dc4e9a798cf83534abec939b
Mijo Rados <mijo.rados@hotmail.com>
2020-12-04 20:58:22 +0100

    Additions Methode implementiert.

--- src/main/java/com/mycompany/taschenrechner/Tascchenrechner.java
+++ src/main/java/com/mycompany/taschenrechner/Tascchenrechner.java
@@ -1,6 +1,4 @@
-
 package com.mycompany.taschenrechner;
-
 
 public class Tascchenrechner {
 
@@ -8,8 +6,11 @@ public class Tascchenrechner {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        
+
     }
-    
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
 }
